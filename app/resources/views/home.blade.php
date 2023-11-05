5b0a5cd627df9c996e5879eb00725ea2f6f638f0
Marucci Maximo <marucci.maximo@gmail.com>
2023-11-05 13:28:58 -0300

    feat #4: improve orders list
    - General filter
    - Items per page
    - Pagination
    - Sorted by fields

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -2,23 +2,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<div class="container mx-auto px-4 sm:px-8">
-    <div class="py-8">
-        <div>
-            <h2 class="text-2xl font-semibold leading-tight">Order</h2>
-        </div>
-        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-            <div
-                class="inline-block min-w-full shadow-md rounded-lg overflow-hidden"
-            >
-                @livewire('orders-list')
+    <div class="container mx-auto px-4 sm:px-8">
+        <div class="py-8">
+            <div>
+                <h2 class="text-2xl font-semibold leading-tight">Orders</h2>
+            </div>
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
+                    @livewire('orders-list')
+                </div>
             </div>
         </div>
     </div>
-</div></div>
+    @livewireScripts
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </body>
 </html>
 
